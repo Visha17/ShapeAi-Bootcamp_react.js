a347e913a711c8b86dd41de1cffc24f0b56ecf12
Visha17 <69953716+Visha17@users.noreply.github.com>
2021-06-27 20:52:46 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright by ShapeAi @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and react.js</h1>
+      <p>
+        A basic web dev react js Bootcamp. I have successfully completed the
+        basic web development and react js bootcamp and have learnt the basics ,
+        All thanks to ShapeAI and specially to Shaurya sir for always being
+        there to help solve our doubts and helping us learn .
+      </p>
+    </div>
+  );
+}
+
+export default Info;
+
